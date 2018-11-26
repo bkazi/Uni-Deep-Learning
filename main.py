c0a1bb36b035152d5b48410cb4a27c75e7d62a2f
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-11-26 19:11:31 +0000

    Use and train on shallow network

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ import numpy as np
 import tensorflow as tf
 
 from utils import get_data
+from shallow_nn import shallow_nn
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('max-steps', 10000,
+tf.app.flags.DEFINE_integer('max-steps', 100,
                             'Number of mini-batches to train on. (default: %(default)d)')
 tf.app.flags.DEFINE_integer('log-frequency', 10,
                             'Number of steps between logging results to the console and saving summaries (default: %(default)d)')
@@ -24,9 +25,11 @@ tf.app.flags.DEFINE_integer(
 tf.app.flags.DEFINE_float('learning-rate', 5e-5,
                           'Learning rate (default: %(default)d)')
 tf.app.flags.DEFINE_integer(
-    'input-width', 80, 'Image width (default: %(default)d)')
+    'input-width', 80, 'Input width (default: %(default)d)')
 tf.app.flags.DEFINE_integer(
-    'input-height', 80, 'Image height (default: %(default)d)')
+    'input-height', 80, 'Input height (default: %(default)d)')
+tf.app.flags.DEFINE_integer(
+    'input-channels', 1, 'Input channels (default: %(default)d)')
 tf.app.flags.DEFINE_integer(
     'num-classes', 10, 'Number of classes (default: %(default)d)')
 tf.app.flags.DEFINE_string('log-dir', '{cwd}/logs/'.format(cwd=os.getcwd()),
@@ -38,13 +41,52 @@ run_log_dir = os.path.join(FLAGS.log_dir,
                                                         lr=FLAGS.learning_rate))
 
 
-def main():
-    train_data, train_labels = get_data()
+def main(_):
 
     with tf.variable_scope('inputs'):
         x = tf.placeholder(
-            tf.float32, [None, FLAGS.img_width, FLAGS.img_height])
+            tf.float32, [None, FLAGS.input_width, FLAGS.input_height, FLAGS.input_channels])
         y = tf.placeholder(tf.float32, [None, FLAGS.num_classes])
+
+    y_out = shallow_nn(x)
+
+    with tf.variable_scope('cross_entropy'):
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_out))
+
+    optimiser = tf.train.AdamOptimizer(
+        FLAGS.learning_rate, name="AdamOpt").minimize(cross_entropy)
+
+    correct_prediction = tf.equal(
+        tf.argmax(y, axis=1), tf.argmax(y_out, axis=1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    train_data, train_labels = get_data()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for epoch in range(FLAGS.max_steps):
+
+            permutation = np.random.permutation(len(train_data))
+            shuffled_train_data = train_data[permutation]
+            shuffled_train_labels = train_labels[permutation]
+            train_data_batched = np.array_split(
+                shuffled_train_data, FLAGS.batch_size)
+            train_labels_batched = np.array_split(
+                shuffled_train_labels, FLAGS.batch_size)
+
+            for i, batch in enumerate(train_data_batched):
+                step = epoch * len(train_data_batched) + i
+
+                sess.run(optimiser, feed_dict={
+                    x: batch, y: train_labels_batched[i]})
+
+                if step % FLAGS.log_frequency == 0:
+                    validation_accuracy = sess.run(
+                        accuracy, feed_dict={x: batch, y: train_labels_batched[i]})
+                    print('step %d, accuracy on validation batch: %g' %
+                          (step, validation_accuracy))
 
 
 if __name__ == '__main__':
