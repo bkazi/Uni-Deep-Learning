8753a7a6e67299b32e7b2730f19b1c6d8e4e83ac
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-11-26 13:17:39 +0000

    moar set up

--- main.py
+++ main.py
@@ -1,15 +1,51 @@
-import pickle
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
 import numpy as np
 import tensorflow as tf
 
-from utils import melspectrogram
+from utils import get_data
 
-with open('music_genres_dataset.pkl', 'rb') as f:
-    train_set = pickle.load(f)
-    test_set = pickle.load(f)
+FLAGS = tf.app.flags.FLAGS
 
-train_set_data = map(lambda x: np.array(
-    melspectrogram(x)), train_set["data"])
-train_set_labels = np.eye(10)[train_set["labels"]]
+tf.app.flags.DEFINE_integer('max-steps', 10000,
+                            'Number of mini-batches to train on. (default: %(default)d)')
+tf.app.flags.DEFINE_integer('log-frequency', 10,
+                            'Number of steps between logging results to the console and saving summaries (default: %(default)d)')
+tf.app.flags.DEFINE_integer('save-model', 1000,
+                            'Number of steps between model saves (default: %(default)d)')
 
-print(train_set_labels)
+# Optimisation hyperparameters
+tf.app.flags.DEFINE_integer(
+    'batch-size', 16, 'Number of examples per mini-batch (default: %(default)d)')
+tf.app.flags.DEFINE_float('learning-rate', 5e-5,
+                          'Learning rate (default: %(default)d)')
+tf.app.flags.DEFINE_integer(
+    'input-width', 80, 'Image width (default: %(default)d)')
+tf.app.flags.DEFINE_integer(
+    'input-height', 80, 'Image height (default: %(default)d)')
+tf.app.flags.DEFINE_integer(
+    'num-classes', 10, 'Number of classes (default: %(default)d)')
+tf.app.flags.DEFINE_string('log-dir', '{cwd}/logs/'.format(cwd=os.getcwd()),
+                           'Directory where to write event logs and checkpoint. (default: %(default)s)')
+
+
+run_log_dir = os.path.join(FLAGS.log_dir,
+                           'exp_bs_{bs}_lr_{lr}'.format(bs=FLAGS.batch_size,
+                                                        lr=FLAGS.learning_rate))
+
+
+def main():
+    train_data, train_labels = get_data()
+
+    with tf.variable_scope('inputs'):
+        x = tf.placeholder(
+            tf.float32, [None, FLAGS.img_width, FLAGS.img_height])
+        y = tf.placeholder(tf.float32, [None, FLAGS.num_classes])
+
+
+if __name__ == '__main__':
+    tf.app.run(main=main)
--- utils.py
+++ utils.py
@@ -1,10 +1,25 @@
+import pickle
 import librosa
 import numpy as np
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
 
 
 def melspectrogram(audio):
-    spec = librosa.stft(audio, n_fft=512, window='hann', hop_length=256, win_length=512, pad_mode='constant')
+    spec = librosa.stft(audio, n_fft=512, window='hann',
+                        hop_length=256, win_length=512, pad_mode='constant')
     mel_basis = librosa.filters.mel(sr=22050, n_fft=512, n_mels=80)
     mel_spec = np.dot(mel_basis, np.abs(spec))
     return np.log(mel_spec + 1e-6)
 
+
+def get_data():
+    with open('music_genres_dataset.pkl', 'rb') as f:
+        train_set = pickle.load(f)
+        test_set = pickle.load(f)
+
+    train_set_data = map(lambda x: np.array(
+        melspectrogram(x)), train_set["data"])
+    train_set_labels = np.eye(FLAGS.num_classes)[train_set["labels"]]
+    return (train_set_data, train_set_labels)
