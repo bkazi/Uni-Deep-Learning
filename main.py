ff606b1c583572879a5a81b5e15a09394568fe34
Bilal Kazi <bilalahmedkazi@hotmail.com>
2018-11-26 12:49:20 +0000

    initial data preprocess

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pickle
+import numpy as np
+import tensorflow as tf
+
+from utils import melspectrogram
 
 with open('music_genres_dataset.pkl', 'rb') as f:
     train_set = pickle.load(f)
     test_set = pickle.load(f)
 
-print(train_set)
+train_set_data = map(lambda x: np.array(
+    melspectrogram(x)), train_set["data"])
+train_set_labels = np.eye(10)[train_set["labels"]]
+
+print(train_set_labels)
